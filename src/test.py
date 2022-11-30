ddb117b16652547bcd4d4499b918ef8b78359a97
Ando <a.r.north23@gmail.com>
2022-11-30 19:55:09 +0000

    1) created abort clauses for each method 2) added DELETE request and response 3) updated tests to PUT an array of videos DELETE and GET

--- src/test.py
+++ src/test.py
@@ -2,12 +2,16 @@ import requests
 
 BASE= "http://127.0.0.1:5000/"
 
+data = [{"likes": 10, "name":"ando's funny video", "views" : 121},
+        {"likes": 1420, "name":"how to make paper planes", "views" : 42000},
+        {"likes": 23099, "name":"awesome flip", "views" : 929299}]
 
-response = requests.get(BASE + "video/1")
-print(response.json())
+for i in range(len(data)):
+    response = requests.put(BASE + "video/"+str(i), data[i])
+    print(response.json())
 
-response = requests.put(BASE + "video/1", {"likes": 10, "name":"ando's funny video", "views" : 121})
-print(response.json())
+response = requests.delete(BASE + "video/0")
+print(response)
 
 response = requests.get(BASE + "video/1")
 print(response.json())
