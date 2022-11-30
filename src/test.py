79a34e679d0c0ee5544977774a6f7f467ccb8f7b
Ando <a.r.north23@gmail.com>
2022-11-30 18:46:54 +0000

    1) added POST response 2) added response from local dictionary of names for GET request 3) added tests for GET request

--- src/test.py
+++ src/test.py
@@ -2,6 +2,7 @@ import requests
 
 BASE= "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld")
+response = requests.get(BASE + "helloworld/ando")
+response = requests.get(BASE + "helloworld/bob")
 
 print(response.json())
